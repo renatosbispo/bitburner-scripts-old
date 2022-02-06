b558ed30d52c8018af701ff1184a9a83e7e50c6d
Renato Bispo <renatosilvabispo@outlook.com>
2022-02-06 05:53:57 -0300

    Implement hackable servers discovery

---
+++ src/scripts/hgw-1/prepare.js
@@ -0,0 +1,84 @@
+/** @param {import("../..").NS } ns */
+
+function getAllServers(ns) {
+  const allServers = [];
+
+  const findServers = (
+    currentServerHostname = 'home',
+    parentServerHostname = null
+  ) => {
+    const neighborsServersHostnames = ns.scan(currentServerHostname);
+
+    for (let neighborServerHostname of neighborsServersHostnames) {
+      if (neighborServerHostname === parentServerHostname) {
+        continue;
+      }
+
+      const neighborServer = ns.getServer(neighborServerHostname);
+      allServers.push(neighborServer);
+
+      findServers(neighborServerHostname, currentServerHostname);
+    }
+  };
+
+  findServers();
+
+  return allServers;
+}
+
+function getAvailablePortOpeners(ns) {
+  const portOpeners = [
+    {
+      filename: 'BruteSSH.exe',
+      run: ns.brutessh,
+    },
+    {
+      filename: 'FTPCrack.exe',
+      run: ns.ftpcrack,
+    },
+    {
+      filename: 'HTTPWorm.exe',
+      run: ns.httpworm,
+    },
+    {
+      filename: 'relaySMTP.exe',
+      run: ns.relaysmtp,
+    },
+    {
+      filename: ' SQLInject.exe',
+      run: ns.sqlinject,
+    },
+  ];
+
+  return portOpeners.filter(({ filename }) => ns.fileExists(filename, 'home'));
+}
+
+function getHackableServers(ns) {
+  const isHackableServer = (server) => {
+    const {
+      hasAdminRights,
+      numOpenPortsRequired,
+      openPortCount,
+      requiredHackingSkill,
+    } = server;
+
+    const playerHackingLevel = ns.getHackingLevel();
+    const totalOpenPortsRequired = numOpenPortsRequired + openPortCount;
+    const availablePortOpenersCount = getAvailablePortOpeners(ns).length;
+
+    return (
+      playerHackingLevel >= requiredHackingSkill &&
+      (hasAdminRights || availablePortOpenersCount >= totalOpenPortsRequired)
+    );
+  };
+
+  const hackableServers = getAllServers(ns).filter((server) =>
+    isHackableServer(server)
+  );
+
+  return hackableServers;
+}
+
+export async function main(ns) {
+  console.log(getHackableServers(ns));
+}
