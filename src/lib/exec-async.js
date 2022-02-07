a9c8cb93805608ef65f40f8b22094d3ee5e5aaa5
Renato Bispo <renatosilvabispo@outlook.com>
2022-02-07 15:42:29 -0300

    Turn execAsync into a class

--- src/lib/exec-async.js
+++ src/lib/exec-async.js
@@ -1,28 +1,37 @@
 /** @param {import("..").NS } ns */
 
-export default async function execAsync(ns, portNumber, ...execArgs) {
-  const pid = ns.exec(...execArgs);
+export default class ExecAsync {
+  constructor(ns, portNumber) {
+    this.ns = ns;
+    this.portNumber = portNumber;
 
-  if (!pid) {
-    const error = ns.getScriptLogs().pop();
-
-    return { error };
+    this.execAsync = this.execAsync.bind(this);
   }
 
-  const port = ns.getPortHandle(portNumber);
+  async execAsync(...execArgs) {
+    const pid = this.ns.exec(...execArgs);
 
-  while (port.empty()) {
-    await ns.sleep(1000);
+    if (!pid) {
+      const error = this.ns.getScriptLogs().pop();
+
+      return { error };
+    }
+
+    const port = this.ns.getPortHandle(this.portNumber);
+
+    while (port.empty()) {
+      await this.ns.sleep(1000);
+    }
+
+    const response = port.read();
+    let data;
+
+    try {
+      data = JSON.parse(response);
+    } catch (_) {
+      data = response;
+    }
+
+    return { data };
   }
-
-  const response = port.read();
-  let data;
-
-  try {
-    data = JSON.parse(response);
-  } catch (_) {
-    data = response;
-  }
-
-  return { data };
 }
