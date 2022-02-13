618d26df28d30d8daf1bec1a8fe3009f7d8d5e4e
Renato Bispo <renatosilvabispo@outlook.com>
2022-02-12 22:24:17 -0300

    Create script for getting crackable servers

---
+++ src/scripts/get-crackable-servers-hostnames.js
@@ -0,0 +1,22 @@
+import ExecAsyncAdapter from '../lib/exec-async-adapter';
+import PortOpenersManager from '../lib/port-openers-manager';
+
+/** @param {import("..").NS } ns */
+export async function main(ns) {
+  const [portNumber, serializedServersHostnames] = ns.args;
+  const { adapt } = new ExecAsyncAdapter(ns, portNumber);
+  const { getAvailablePortOpeners } = new PortOpenersManager(ns);
+
+  await adapt(async () => {
+    const serversHostnames = JSON.parse(serializedServersHostnames);
+    const availablePortOpeners = getAvailablePortOpeners();
+
+    const crackableServers = serversHostnames.filter((serverHostname) => {
+      const numPortsRequired = ns.getServerNumPortsRequired(serverHostname);
+
+      return availablePortOpeners.length >= numPortsRequired;
+    });
+
+    return crackableServers;
+  });
+}
