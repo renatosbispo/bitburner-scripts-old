beac924fee8fa71dc2387f7d5f72c55f58104d63
Renato Bispo <renatosilvabispo@outlook.com>
2022-02-12 22:46:32 -0300

    Create script for getting hackable servers

---
+++ src/scripts/get-hackable-servers-hostnames.js
@@ -0,0 +1,23 @@
+import ExecAsyncAdapter from '../lib/exec-async-adapter';
+
+/** @param {import("..").NS } ns */
+export async function main(ns) {
+  const [portNumber, serializedServersHostnames] = ns.args;
+
+  const { adapt } = new ExecAsyncAdapter(ns, portNumber);
+
+  await adapt(async () => {
+    const serversHostnames = JSON.parse(serializedServersHostnames);
+
+    const hackableServersHostnames = serversHostnames.filter(
+      (serverHostname) => {
+        const serverRequiredHackingLevel =
+          ns.getServerRequiredHackingLevel(serverHostname);
+
+        return ns.getHackingLevel() >= serverRequiredHackingLevel;
+      }
+    );
+
+    return hackableServersHostnames;
+  });
+}
